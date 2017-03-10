eb7f649ce43b87b768c0efd516262add23abfc6a
Prima20 <primakurnia71@gmail.com>
2017-03-10 21:42:20 +0800

    Pernyataan edited
    edited

---
+++ src/latihan/BelPac.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belpac;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BelPac {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[][] array = new String[][]{
+        {"#","#","#","#","#","#","#","#","#","#"},
+        {"#","<"," "," "," ","@"," "," "," ","#"},
+        {"#","#"," "," "," "," ","#","#","#","#"},
+        {"#"," "," "," "," "," "," "," "," ","#"},
+        {"#","#"," ","@","@"," ","#"," "," ","#"},
+        {"#"," "," "," "," "," ","#","#","#","#"},
+        {"#"," "," "," "," "," ","@","#"," ","#"},
+        {"#"," ","#","#"," "," "," ","@"," ","#"},
+        {"#"," "," ","#","#"," "," "," "," ","#"},
+        {"#","#","#","#","#","#","#","#","#","#"}};
+        Pacman game = new Pacman();
+        game.x= 1;
+        game.y = 1;
+        
+        boolean t = true;
+        game.step = 20;
+        game.score = 0;
+        do{
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println("");
+        }
+        game.jumStep();
+        game.showScore();
+        System.out.print("Enter move : (w,a,s,d q for quit > ");
+        char a = scan.next().charAt(0);
+        if(a == 'q'){
+            t = false;
+        }
+        array[game.x][game.y] = " "; 
+        game.move(a);
+        if(array[game.x][game.y] == " "){
+            array[game.x][game.y] = game.objek;
+        }
+        else if(array[game.x][game.y] == "@"){
+             array[game.x][game.y] = game.objek;
+             game.makan();
+        }
+        else if(array[game.x][game.y] == "#"){
+            array[game.x][game.y] = "#";
+        }
+        if(game.step == 0 && game.score!=5){
+            for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println("");
+        }
+            System.out.println("YOU LOSE");
+            t = false;
+        }
+        else if(game.score == 5){
+            for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println("");
+        }
+            System.out.println("YOU WIN");
+            t = false;
+        }
+      }while(t);
+    }
+    
+}
---
+++ src/latihan/Pacman.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belpac;
+
+public class Pacman {
+    public int step;
+    public String objek;
+    public int score;
+    public int x;
+    public int y;
+    
+    
+    public void jumStep(){
+        step -= 1;
+        System.out.println("Step left : " + step);
+    }
+    public void move(char c){
+        if(c == 'w'){
+            x -=1;
+            y += 0;
+            objek = "v";
+        }
+        if(c == 's'){
+            x +=1;
+            y += 0;
+            objek = "^";
+        }
+        if(c == 'd'){
+            y +=1;
+            x +=0;
+            objek = "<";
+        }
+        if(c == 'a'){
+            y -=1;
+            x +=0;
+            objek = ">";
+        }
+    }
+    public void makan(){
+        score += 1;
+    }
+    public void showScore(){
+        System.out.println("Score : " + score);
+    }
+}
